f291c03b36d85c9b2d570872596ca8df0c4fde01
João Eduardo Saczuk Machado Pereira <joaoeduardo.smp@gmail.com>
2025-01-29 10:54:18 -0300

    Somewhat decent approach using SDL_WasInit but it doesn't work because SDL also uses refcounts internally and initializes subsystems automatically depending on the flag passed.
    Fucking C

--- src/init.rs
+++ src/init.rs
@@ -12,94 +12,128 @@ use core::sync::atomic::{AtomicBool, Ordering};
 use sdl3_sys as sys;
 
 static IS_SDL_INITIALIZED: AtomicBool = AtomicBool::new(false);
+const INITIALIZED: bool = true;
+const UNINITIALIZED: bool = false;
 
-const LOCKED: bool = true;
-const UNLOCKED: bool = false;
+#[derive(Clone)]
+pub struct Sdl(Arc<SdlDrop>, PhantomData<*const ()>);
 
-pub struct Sdl {
-    drop: Arc<SdlDrop>,
-    video: Arc<AtomicBool>,
-    _marker: PhantomData<*const ()>,
-}
+#[derive(Clone)]
+pub struct AudioSubsystem(Arc<Subsystem<{ sys::init::SDL_INIT_AUDIO }>>);
+
+#[derive(Clone)]
+pub struct CameraSubsystem(Arc<Subsystem<{ sys::init::SDL_INIT_CAMERA }>>);
+
+#[derive(Clone)]
+pub struct EventsSubsystem(Arc<Subsystem<{ sys::init::SDL_INIT_EVENTS }>>);
+
+#[derive(Clone)]
+pub struct GamepadSubsystem(Arc<Subsystem<{ sys::init::SDL_INIT_GAMEPAD }>>);
+
+#[derive(Clone)]
+pub struct HapticSubsystem(Arc<Subsystem<{ sys::init::SDL_INIT_HAPTIC }>>);
+
+#[derive(Clone)]
+pub struct JoystickSubsystem(Arc<Subsystem<{ sys::init::SDL_INIT_JOYSTICK }>>);
+
+#[derive(Clone)]
+pub struct VideoSubsystem(Arc<Subsystem<{ sys::init::SDL_INIT_VIDEO }>>);
+
+#[derive(Clone)]
+pub struct SensorSubsystem(Arc<Subsystem<{ sys::init::SDL_INIT_SENSOR }>>);
 
 impl Sdl {
-    /// SAFETY: Must be called from the main thread.
+    /// SAFETY: This must be called from the main thread.
     pub unsafe fn init() -> Result<Self, Error> {
-        let drop = SdlDrop::new()?;
-        Ok(Self {
-            drop: Arc::new(drop),
-            video: Arc::new(AtomicBool::new(false)),
-            _marker: PhantomData,
-        })
-    }
-
-    pub fn video(&self) -> Result<VideoSubsystem, Error> {
-        Subsystem::init(&self.drop, &self.video).map(VideoSubsystem)
-    }
-}
-
-pub struct VideoSubsystem(Subsystem<{ sys::init::SDL_INIT_VIDEO }>);
-
-struct SdlDrop;
-
-impl SdlDrop {
-    /// SAFETY: Must be called from the main thread.
-    unsafe fn new() -> Result<Self, Error> {
-        let cmp = IS_SDL_INITIALIZED.compare_exchange(
-            UNLOCKED,
-            LOCKED,
+        let res = IS_SDL_INITIALIZED.compare_exchange(
+            UNINITIALIZED,
+            INITIALIZED,
             Ordering::Relaxed,
             Ordering::Relaxed,
         );
-
-        if cmp.is_err() {
-            return Err(Error(String::from("SDL is already initialized")));
+        if res.is_err() {
+            return Err(Error(String::from("SDL is already initialized.")));
         }
 
-        if !sys::init::SDL_Init(0) {
-            IS_SDL_INITIALIZED.store(false, Ordering::Relaxed);
+        let result = unsafe { sys::init::SDL_Init(0) };
+        if !result {
+            let _ = IS_SDL_INITIALIZED.compare_exchange(
+                INITIALIZED,
+                UNINITIALIZED,
+                Ordering::Relaxed,
+                Ordering::Relaxed,
+            );
             return Err(Error::from_sdl());
         }
 
-        Ok(Self)
+        Ok(Self(Arc::new(SdlDrop), PhantomData))
+    }
+
+    pub fn audio(&self) -> Result<AudioSubsystem, Error> {
+        Subsystem::init(&self.0).map(Arc::new).map(AudioSubsystem)
+    }
+
+    pub fn camera(&self) -> Result<CameraSubsystem, Error> {
+        Subsystem::init(&self.0).map(Arc::new).map(CameraSubsystem)
+    }
+
+    pub fn events(&self) -> Result<EventsSubsystem, Error> {
+        Subsystem::init(&self.0).map(Arc::new).map(EventsSubsystem)
+    }
+
+    pub fn gamepad(&self) -> Result<GamepadSubsystem, Error> {
+        Subsystem::init(&self.0).map(Arc::new).map(GamepadSubsystem)
+    }
+
+    pub fn haptic(&self) -> Result<HapticSubsystem, Error> {
+        Subsystem::init(&self.0).map(Arc::new).map(HapticSubsystem)
+    }
+
+    pub fn joystick(&self) -> Result<JoystickSubsystem, Error> {
+        Subsystem::init(&self.0).map(Arc::new).map(JoystickSubsystem)
+    }
+
+    pub fn video(&self) -> Result<VideoSubsystem, Error> {
+        Subsystem::init(&self.0).map(Arc::new).map(VideoSubsystem)
+    }
+
+    pub fn sensor(&self) -> Result<SensorSubsystem, Error> {
+        Subsystem::init(&self.0).map(Arc::new).map(SensorSubsystem)
     }
 }
 
-impl Drop for SdlDrop {
-    fn drop(&mut self) {
-        unsafe { sys::init::SDL_Quit() };
-        IS_SDL_INITIALIZED.store(false, Ordering::Relaxed);
-    }
-}
-
-struct Subsystem<const INIT_FLAG: u32> {
-    flag: Arc<AtomicBool>,
-    drop: Arc<SdlDrop>,
-    _marker: PhantomData<*const ()>,
-}
+struct Subsystem<const INIT_FLAG: u32>(Arc<SdlDrop>, PhantomData<*const ()>);
 
 impl<const INIT_FLAG: u32> Subsystem<INIT_FLAG> {
-    fn init(drop: &Arc<SdlDrop>, flag: &Arc<AtomicBool>) -> Result<Self, Error> {
-        let cmp = flag.compare_exchange(UNLOCKED, LOCKED, Ordering::Relaxed, Ordering::Relaxed);
-        if cmp.is_err() {
-            return Err(Error(String::from("Subsystem is already initialized")));
+    fn init(sdl: &Arc<SdlDrop>) -> Result<Self, Error> {
+        let was_init = unsafe { sys::init::SDL_WasInit(INIT_FLAG) };
+        if was_init != 0 {
+            return Err(Error(String::from("Subsystem is already initialized.")));
         }
-        let ret = unsafe { sys::init::SDL_InitSubSystem(INIT_FLAG) };
-        if !ret {
-            flag.store(false, Ordering::Relaxed);
+        let result = unsafe { sys::init::SDL_InitSubSystem(INIT_FLAG) };
+        if !result {
             return Err(Error::from_sdl());
         }
-        Ok(Self {
-            flag: Arc::clone(&flag),
-            drop: Arc::clone(&drop),
-            _marker: PhantomData,
-        })
+        Ok(Self(Arc::clone(sdl), PhantomData))
     }
 }
 
 impl<const INIT_FLAG: u32> Drop for Subsystem<INIT_FLAG> {
     fn drop(&mut self) {
-        unsafe { sys::init::SDL_QuitSubSystem(INIT_FLAG) };
-        self.flag.store(false, Ordering::Relaxed);
+        unsafe {
+            let was_init = sys::init::SDL_WasInit(INIT_FLAG);
+            if was_init != 0 {
+                unsafe { sys::init::SDL_QuitSubSystem(INIT_FLAG) };
+            }
+        }
+    }
+}
+
+struct SdlDrop;
+
+impl Drop for SdlDrop {
+    fn drop(&mut self) {
+        unsafe { sys::init::SDL_Quit() };
+        IS_SDL_INITIALIZED.store(true, Ordering::Relaxed);
     }
 }
