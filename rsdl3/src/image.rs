617d4b64ca9027f56d1f221d9976c9562ec284dd
João Eduardo <joaoeduardo.smp@gmail.com>
2025-03-03 14:07:06 -0300

    Load image

---
+++ rsdl3/src/image.rs
@@ -0,0 +1,19 @@
+use std::ffi::CString;
+
+use crate::init::VideoSubsystem;
+use crate::surface::Surface;
+use crate::sys;
+use crate::Error;
+
+impl VideoSubsystem {
+    pub fn load_image(&self, path: &str) -> Result<Surface, Error> {
+        let path = CString::new(path)?;
+        unsafe {
+            let surface = sys::image::IMG_Load(path.as_ptr());
+            if surface.is_null() {
+                return Err(Error::from_sdl());
+            }
+            Ok(Surface::from_mut_ptr(self, surface))
+        }
+    }
+}
